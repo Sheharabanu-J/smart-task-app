a45f9d4bc0d3059f5201b2bdc94b82ed128b3d5a
Shehara Banu <sheharabanu555@gmail.com>
2026-04-28 19:09:20 +0530

    fix supabase ssl

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("smarttask", "postgres", "1507", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
